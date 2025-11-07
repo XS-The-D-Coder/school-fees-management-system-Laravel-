b79c11ede488951464cc6789494299e9bbebdd96
XS-The-D-Coder <flintman19@gmail.com>
2025-11-07 13:04:04 +0300

    Registered the Admin Middleware in the Kernel under routeMiddleware.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,6 +45,12 @@ class Kernel extends HttpKernel
         ],
     ];
 
+
+    protected $routeMiddleware = [
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
+    ];
+
+
     /**
      * The application's middleware aliases.
      *
